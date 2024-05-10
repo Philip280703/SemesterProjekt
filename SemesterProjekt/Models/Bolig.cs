0c6d8bc6bb7f4df479ae8c64c2cb6c7d002b6678
Philip Holm Møller <phmo61881@edu.ucl.dk>
2024-05-10 16:04:04 +0200

    added validation in get/set - P

--- SemesterProjekt/Models/Bolig.cs
+++ SemesterProjekt/Models/Bolig.cs
@@ -8,15 +8,78 @@ namespace SemesterProjekt.Models
 {
     internal class Bolig
     {
-        public int BoligId { get; set; }
+        private int boligId;
+        public int BoligId
+        {
+            get
+            {
+                return boligId;
+            }
+            set
+            {
+                if (value <= 0 || value > 100001)
+                {
+                    throw new Exception("BoligId is out of range");
+                }
+                boligId = value;
+            }
+        }
         public string Adresse { get; set; }
-        public int PostNr { get; set; }
-        public int UdbudsPris { get; set; }
+
+        int postnr;
+        public int PostNr
+        {
+            get
+            {
+                return postnr;
+            }
+            set
+            {
+                if (value <= 1000 || value > 10000 )
+                {
+                    throw new Exception("Postalcode is out of range");
+                }
+                postnr = value;
+            }
+        }
+
+        int udbudspris;
+        public int UdbudsPris
+        {
+            get
+            {
+                return udbudspris;
+            }
+            set
+            {
+                if (value <= 0 || value > 1000000001)
+                {
+                    throw new Exception("Price is out of range");
+                }
+                udbudspris = value;
+            }
+        }
         public int Kvadratmeter { get; set; }
         public int KvmPris { get; set; }
         public int BoligType { get; set; }
         public bool Aktiv {  get; set; }
-        public int SalgsPris { get; set; }
+
+        int salgspris;
+        public int SalgsPris
+        {
+            get
+            {
+                return salgspris;
+            }
+            set
+            {
+                if (value <= 0 || value > 1000000001)
+                {
+                    throw new Exception("Price is out of range");
+                }
+                salgspris = value;
+            }
+        }
         public DateTime SalgsDato { get; set; }
         public int MaeglerId { get; set; }
     }
