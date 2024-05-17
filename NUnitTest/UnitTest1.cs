6475176b32bae3a329f04dfaf87a35c40355d52d
Philip Holm Møller <phmo61881@edu.ucl.dk>
2024-05-17 09:27:26 +0200

    nunit

---
+++ NUnitTest/UnitTest1.cs
@@ -0,0 +1,18 @@
+using SemesterProjekt;
+namespace NUnitTest
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Bolig bolig = new Bolig();
+
+        }
+    }
+}
