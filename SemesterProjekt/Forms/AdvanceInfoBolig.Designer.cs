78596b9136db5b5d0c2909fa7a62cff214fe3a6c
Philip Holm Møller <phmo61881@edu.ucl.dk>
2024-05-21 11:27:50 +0200

    max date

--- SemesterProjekt/Forms/AdvanceInfoBolig.Designer.cs
+++ SemesterProjekt/Forms/AdvanceInfoBolig.Designer.cs
@@ -60,153 +60,162 @@
             // BoligIdLabel
             // 
             BoligIdLabel.AutoSize = true;
-            BoligIdLabel.Location = new Point(48, 33);
+            BoligIdLabel.Location = new Point(55, 44);
             BoligIdLabel.Name = "BoligIdLabel";
-            BoligIdLabel.Size = new Size(44, 15);
+            BoligIdLabel.Size = new Size(57, 20);
             BoligIdLabel.TabIndex = 0;
             BoligIdLabel.Text = "BoligId";
             // 
             // AdresseLabel
             // 
             AdresseLabel.AutoSize = true;
-            AdresseLabel.Location = new Point(44, 62);
+            AdresseLabel.Location = new Point(50, 83);
             AdresseLabel.Name = "AdresseLabel";
-            AdresseLabel.Size = new Size(48, 15);
+            AdresseLabel.Size = new Size(61, 20);
             AdresseLabel.TabIndex = 1;
             AdresseLabel.Text = "Adresse";
             // 
             // BoligIdTextBox
             // 
-            BoligIdTextBox.Location = new Point(98, 30);
+            BoligIdTextBox.Location = new Point(112, 40);
+            BoligIdTextBox.Margin = new Padding(3, 4, 3, 4);
             BoligIdTextBox.Name = "BoligIdTextBox";
             BoligIdTextBox.ReadOnly = true;
-            BoligIdTextBox.Size = new Size(149, 23);
+            BoligIdTextBox.Size = new Size(170, 27);
             BoligIdTextBox.TabIndex = 3;
             // 
             // AdresseTextbox
             // 
-            AdresseTextbox.Location = new Point(98, 59);
+            AdresseTextbox.Location = new Point(112, 79);
+            AdresseTextbox.Margin = new Padding(3, 4, 3, 4);
             AdresseTextbox.Name = "AdresseTextbox";
             AdresseTextbox.ReadOnly = true;
-            AdresseTextbox.Size = new Size(149, 23);
+            AdresseTextbox.Size = new Size(170, 27);
             AdresseTextbox.TabIndex = 4;
             // 
             // PostNrTextbox
             // 
-            PostNrTextbox.Location = new Point(98, 88);
+            PostNrTextbox.Location = new Point(112, 117);
+            PostNrTextbox.Margin = new Padding(3, 4, 3, 4);
             PostNrTextbox.Name = "PostNrTextbox";
             PostNrTextbox.ReadOnly = true;
-            PostNrTextbox.Size = new Size(149, 23);
+            PostNrTextbox.Size = new Size(170, 27);
             PostNrTextbox.TabIndex = 5;
             // 
             // PostNrLabel
             // 
             PostNrLabel.AutoSize = true;
-            PostNrLabel.Location = new Point(48, 91);
+            PostNrLabel.Location = new Point(55, 121);
             PostNrLabel.Name = "PostNrLabel";
-            PostNrLabel.Size = new Size(43, 15);
+            PostNrLabel.Size = new Size(52, 20);
             PostNrLabel.TabIndex = 6;
             PostNrLabel.Text = "PostNr";
             // 
             // UdbudsprisTextbox
             // 
-            UdbudsprisTextbox.Location = new Point(98, 117);
+            UdbudsprisTextbox.Location = new Point(112, 156);
+            UdbudsprisTextbox.Margin = new Padding(3, 4, 3, 4);
             UdbudsprisTextbox.Name = "UdbudsprisTextbox";
             UdbudsprisTextbox.ReadOnly = true;
-            UdbudsprisTextbox.Size = new Size(149, 23);
+            UdbudsprisTextbox.Size = new Size(170, 27);
             UdbudsprisTextbox.TabIndex = 7;
             // 
             // label1
             // 
             label1.AutoSize = true;
-            label1.Location = new Point(24, 120);
+            label1.Location = new Point(27, 160);
             label1.Name = "label1";
-            label1.Size = new Size(67, 15);
+            label1.Size = new Size(84, 20);
             label1.TabIndex = 8;
             label1.Text = "Udbudspris";
             // 
             // KvadratmeterTextbox
             // 
-            KvadratmeterTextbox.Location = new Point(98, 146);
+            KvadratmeterTextbox.Location = new Point(112, 195);
+            KvadratmeterTextbox.Margin = new Padding(3, 4, 3, 4);
             KvadratmeterTextbox.Name = "KvadratmeterTextbox";
             KvadratmeterTextbox.ReadOnly = true;
-            KvadratmeterTextbox.Size = new Size(149, 23);
+            KvadratmeterTextbox.Size = new Size(170, 27);
             KvadratmeterTextbox.TabIndex = 9;
             // 
             // KvadratmeterLabel
             // 
             KvadratmeterLabel.AutoSize = true;
-            KvadratmeterLabel.Location = new Point(14, 149);
+            KvadratmeterLabel.Location = new Point(16, 199);
             KvadratmeterLabel.Name = "KvadratmeterLabel";
-            KvadratmeterLabel.Size = new Size(78, 15);
+            KvadratmeterLabel.Size = new Size(98, 20);
             KvadratmeterLabel.TabIndex = 10;
             KvadratmeterLabel.Text = "Kvadratmeter";
             // 
             // KvmPrisLabel
             // 
             KvmPrisLabel.AutoSize = true;
-            KvmPrisLabel.Location = new Point(274, 33);
+            KvmPrisLabel.Location = new Point(313, 44);
             KvmPrisLabel.Name = "KvmPrisLabel";
-            KvmPrisLabel.Size = new Size(50, 15);
+            KvmPrisLabel.Size = new Size(61, 20);
             KvmPrisLabel.TabIndex = 11;
             KvmPrisLabel.Text = "Kvmpris";
             // 
             // KvmPrisTextBox
             // 
-            KvmPrisTextBox.Location = new Point(330, 30);
+            KvmPrisTextBox.Location = new Point(377, 40);
+            KvmPrisTextBox.Margin = new Padding(3, 4, 3, 4);
             KvmPrisTextBox.Name = "KvmPrisTextBox";
             KvmPrisTextBox.ReadOnly = true;
-            KvmPrisTextBox.Size = new Size(149, 23);
+            KvmPrisTextBox.Size = new Size(170, 27);
             KvmPrisTextBox.TabIndex = 12;
             // 
             // BoligTypeTextbox
             // 
-            BoligTypeTextbox.Location = new Point(330, 59);
+            BoligTypeTextbox.Location = new Point(377, 79);
+            BoligTypeTextbox.Margin = new Padding(3, 4, 3, 4);
             BoligTypeTextbox.Name = "BoligTypeTextbox";
             BoligTypeTextbox.ReadOnly = true;
-            BoligTypeTextbox.Size = new Size(149, 23);
+            BoligTypeTextbox.Size = new Size(170, 27);
             BoligTypeTextbox.TabIndex = 13;
             // 
             // BoligTypeLabel
             // 
             BoligTypeLabel.AutoSize = true;
-            BoligTypeLabel.Location = new Point(266, 62);
+            BoligTypeLabel.Location = new Point(304, 83);
             BoligTypeLabel.Name = "BoligTypeLabel";
-            BoligTypeLabel.Size = new Size(58, 15);
+            BoligTypeLabel.Size = new Size(75, 20);
             BoligTypeLabel.TabIndex = 14;
             BoligTypeLabel.Text = "BoligType";
             // 
             // AktivTextbox
             // 
-            AktivTextbox.Location = new Point(330, 88);
+            AktivTextbox.Location = new Point(377, 117);
+            AktivTextbox.Margin = new Padding(3, 4, 3, 4);
             AktivTextbox.Name = "AktivTextbox";
             AktivTextbox.ReadOnly = true;
-            AktivTextbox.Size = new Size(149, 23);
+            AktivTextbox.Size = new Size(170, 27);
             AktivTextbox.TabIndex = 15;
             // 
             // AktivLabel
             // 
             AktivLabel.AutoSize = true;
-            AktivLabel.Location = new Point(286, 91);
+            AktivLabel.Location = new Point(327, 121);
             AktivLabel.Name = "AktivLabel";
-            AktivLabel.Size = new Size(34, 15);
+            AktivLabel.Size = new Size(42, 20);
             AktivLabel.TabIndex = 17;
             AktivLabel.Text = "Aktiv";
             // 
             // MaglerIdTextbox
             // 
-            MaglerIdTextbox.Location = new Point(330, 117);
+            MaglerIdTextbox.Location = new Point(377, 156);
+            MaglerIdTextbox.Margin = new Padding(3, 4, 3, 4);
             MaglerIdTextbox.Name = "MaglerIdTextbox";
             MaglerIdTextbox.ReadOnly = true;
-            MaglerIdTextbox.Size = new Size(149, 23);
+            MaglerIdTextbox.Size = new Size(170, 27);
             MaglerIdTextbox.TabIndex = 18;
             // 
             // MæglerIdLabel
             // 
             MæglerIdLabel.AutoSize = true;
-            MæglerIdLabel.Location = new Point(266, 120);
+            MæglerIdLabel.Location = new Point(304, 160);
             MæglerIdLabel.Name = "MæglerIdLabel";
-            MæglerIdLabel.Size = new Size(58, 15);
+            MæglerIdLabel.Size = new Size(73, 20);
             MæglerIdLabel.TabIndex = 19;
             MæglerIdLabel.Text = "MæglerId";
             // 
@@ -219,52 +228,58 @@
             panel1.Controls.Add(SalgsDatoLbl);
             panel1.Controls.Add(dateTimePicker1);
             panel1.Controls.Add(SælgBoligLabel);
-            panel1.Location = new Point(503, 6);
+            panel1.Location = new Point(575, 8);
+            panel1.Margin = new Padding(3, 4, 3, 4);
             panel1.Name = "panel1";
-            panel1.Size = new Size(300, 216);
+            panel1.Size = new Size(343, 288);
             panel1.TabIndex = 20;
             // 
             // SælgBolig
             // 
             SælgBolig.Dock = DockStyle.Bottom;
             SælgBolig.FlatStyle = FlatStyle.Flat;
-            SælgBolig.Location = new Point(0, 182);
+            SælgBolig.Location = new Point(0, 243);
+            SælgBolig.Margin = new Padding(3, 4, 3, 4);
             SælgBolig.Name = "SælgBolig";
-            SælgBolig.Size = new Size(300, 34);
+            SælgBolig.Size = new Size(343, 45);
             SælgBolig.TabIndex = 5;
             SælgBolig.Text = "Sælg Bolig";
             SælgBolig.UseVisualStyleBackColor = true;
             // 
             // Salgspris
             // 
-            Salgspris.Location = new Point(16, 137);
+            Salgspris.Location = new Point(18, 183);
+            Salgspris.Margin = new Padding(3, 4, 3, 4);
             Salgspris.Name = "Salgspris";
-            Salgspris.Size = new Size(267, 23);
+            Salgspris.Size = new Size(305, 27);
             Salgspris.TabIndex = 4;
             // 
             // SalgsPrisLbl
             // 
-            SalgsPrisLbl.Location = new Point(0, 105);
+            SalgsPrisLbl.Location = new Point(0, 140);
             SalgsPrisLbl.Name = "SalgsPrisLbl";
-            SalgsPrisLbl.Size = new Size(297, 29);
+            SalgsPrisLbl.Size = new Size(339, 39);
             SalgsPrisLbl.TabIndex = 3;
             SalgsPrisLbl.Text = "Salgspris";
             SalgsPrisLbl.TextAlign = ContentAlignment.MiddleCenter;
             // 
             // SalgsDatoLbl
             // 
-            SalgsDatoLbl.Location = new Point(0, 47);
+            SalgsDatoLbl.Location = new Point(0, 63);
             SalgsDatoLbl.Name = "SalgsDatoLbl";
-            SalgsDatoLbl.Size = new Size(297, 29);
+            SalgsDatoLbl.Size = new Size(339, 39);
             SalgsDatoLbl.TabIndex = 2;
             SalgsDatoLbl.Text = "Salgsdato";
             SalgsDatoLbl.TextAlign = ContentAlignment.MiddleCenter;
             // 
             // dateTimePicker1
             // 
-            dateTimePicker1.Location = new Point(16, 79);
+            dateTimePicker1.Location = new Point(18, 105);
+            dateTimePicker1.Margin = new Padding(3, 4, 3, 4);
+            dateTimePicker1.MaxDate = new DateTime(2025, 12, 31, 0, 0, 0, 0);
+            dateTimePicker1.MinDate = new DateTime(1850, 1, 1, 0, 0, 0, 0);
             dateTimePicker1.Name = "dateTimePicker1";
-            dateTimePicker1.Size = new Size(267, 23);
+            dateTimePicker1.Size = new Size(305, 27);
             dateTimePicker1.TabIndex = 1;
             // 
             // SælgBoligLabel
@@ -273,16 +288,17 @@
             SælgBoligLabel.Dock = DockStyle.Top;
             SælgBoligLabel.Location = new Point(0, 0);
             SælgBoligLabel.Name = "SælgBoligLabel";
-            SælgBoligLabel.Size = new Size(300, 23);
+            SælgBoligLabel.Size = new Size(343, 31);
             SælgBoligLabel.TabIndex = 0;
             SælgBoligLabel.Text = "Sælg Bolig";
             SælgBoligLabel.TextAlign = ContentAlignment.MiddleCenter;
             // 
             // TilbageButton
             // 
-            TilbageButton.Location = new Point(7, 218);
+            TilbageButton.Location = new Point(12, 281);
+            TilbageButton.Margin = new Padding(3, 4, 3, 4);
             TilbageButton.Name = "TilbageButton";
-            TilbageButton.Size = new Size(75, 23);
+            TilbageButton.Size = new Size(86, 31);
             TilbageButton.TabIndex = 21;
             TilbageButton.Text = "Tilbage";
             TilbageButton.UseVisualStyleBackColor = true;
@@ -290,10 +306,10 @@
             // 
             // AdvanceInfoBolig
             // 
-            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
             BackColor = Color.White;
-            ClientSize = new Size(815, 244);
+            ClientSize = new Size(931, 325);
             Controls.Add(TilbageButton);
             Controls.Add(panel1);
             Controls.Add(MæglerIdLabel);
@@ -314,6 +330,7 @@
             Controls.Add(BoligIdTextBox);
             Controls.Add(AdresseLabel);
             Controls.Add(BoligIdLabel);
+            Margin = new Padding(3, 4, 3, 4);
             Name = "AdvanceInfoBolig";
             Text = "Form3";
             panel1.ResumeLayout(false);
