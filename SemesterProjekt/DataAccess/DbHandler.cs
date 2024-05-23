e9e38b204db050ce42104442ecbc69e87eb1672f
Philip Holm Møller <phmo61881@edu.ucl.dk>
2024-05-23 11:48:19 +0200

    sdgnsfn

--- SemesterProjekt/DataAccess/DbHandler.cs
+++ SemesterProjekt/DataAccess/DbHandler.cs
@@ -17,7 +17,7 @@ namespace SemesterProjekt.DataAccess
         string ConnectionString;
         public DbHandler() 
         { 
-            ConnectionString = System.Configuration.ConfigurationManager.ConnectionStrings["defaultB"].ToString();
+            ConnectionString = System.Configuration.ConfigurationManager.ConnectionStrings["defaultP"].ToString();
         }
 
 
